eb339b923336587e9ae03908129078857bc5d24b
Prakash Kumar Jha <38212086+pkj002@users.noreply.github.com>
2026-04-13 07:53:17 -0700

    Update Figure 3.py

--- CommSust/Codes/Figure 3.py
+++ CommSust/Codes/Figure 3.py
@@ -6,6 +6,7 @@ import xarray as xr
 import geopandas as gpd
 
 # --- File paths (copy 'plot_data_published' directory inside path dir) ---
+# Download "plot_data_published" directory (https://ucmerced.box.com/s/u9ntcj58lii519std4u1cfi8gkl07h5k)
 path = "path_to_project_root" 
 
 # GRIDMET
